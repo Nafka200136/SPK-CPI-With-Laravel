f909af4340ea1be179635ebc93847e39b2e4c632
nafard <G231200136@student.usm.ac.id>
2024-06-05 20:41:47 +0700

    update background to bg-blue-950

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,11 +16,11 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-blue-950">
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                <div>
+                <div class="flex justify-center items-center">
                     <a href="/">
-                        <img src="{{ asset('assets/img/Favicon-FTIK-USM.png') }}" class="w-1/2 h-1/2 mx-auto" alt="logo">
+                        <img src="{{ asset('assets/img/Favicon-FTIK-USM.png') }}" class="w-24 mr-2" alt="logo">
                     </a>
                 </div>
                 {{ $slot }}
