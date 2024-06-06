2a012cb41d0d7bca63edd0a24c7b45d716811b8d
nafard <G231200136@student.usm.ac.id>
2024-06-06 13:20:04 +0700

    Add middleware to CriteriaCOntroller

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\backend\KinerjaController;
 use App\Http\Controllers\frontend\CandidateController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\CheckCriteriaWeights;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,7 +20,7 @@ Route::get('/dashboard', function () {
 
 Route::middleware('auth')->group(function () {
     Route::resource('alternatif', AlternatifController::class);
-    Route::resource('kriteria', CriteriaController::class);
+    Route::resource('kriteria', CriteriaController::class)->middleware(CheckCriteriaWeights::class);
     Route::resource('kinerja', KinerjaController::class);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
